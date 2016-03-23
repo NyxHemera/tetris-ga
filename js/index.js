ec51935589bce2fc1602def238131d5111f344c3
Jordan Rust <jordan.rust92@gmail.com>
2016-03-23 17:30:38 -0400

    Added basic canvas

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// Create canvas
+var canvas;
+var ctx;
+
+
+function createCanvas() {
+	canvas = document.createElement("canvas");
+	ctx = canvas.getContext("2d");
+	canvas.width = 512;
+	canvas.height = 480;
+	document.body.appendChild(canvas);
+
+	render();
+}
+
+function render() {
+	ctx.fillStyle="#FFFFFF";
+	ctx.fillRect(0,0,canvas.width,canvas.height);	
+}
