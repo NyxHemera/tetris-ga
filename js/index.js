128b3d3c221eb4ea5dd7e4ece46c90ed20e8747d
Jordan Rust <jordan.rust92@gmail.com>
2016-03-28 21:50:14 -0400

    Implemented lateral and downward movement for pieces. Realized that rotations worked horribly, so I refactored the code handling both rotation and the positions of the bcd blocks. This fixed all rotation issues and made the Piece code much more readable and managable.

--- js/index.js
+++ js/index.js
@@ -3,153 +3,50 @@ var canvas;
 var ctx;
 
 var bw = 20;
+var currentPiece;
+
+// Update speed in ms
+var speed = 300;
+var loopSpeed = 60;
+var runTime = 0;
 
 // Tetrads
-
 class Piece {
 	constructor() {
 		// Rotation enum
-		this.rotation = {
-			DOWN: 0,
-			LEFT: 1,
-			UP: 2,
-			RIGHT: 3,
-			properties: {
-				0: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					}
-				},
-				1: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					}
-				},
-				2: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					}
-				},
-				3: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					}
-				}
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		// Down: 0, Left: 1, Up: 2, Right: 3
+		this.currentRotation = 0;
+		this.deltaPos = [
+			[], //down
+			[], //left
+			[], //up
+			[] //right
+		];
+	}
+	move(x, y) {
+			this.blockPos[0] += x;
+			this.blockPos[1] += y;
+			this.updateBCD(this.blockPos[0], this.blockPos[1]);
+		}
+		// Iterate through the rotation states
+	rotate(direction) {
+			if (direction) {
+				// Rotate clockwise
+				this.currentRotation < 3 ? this.currentRotation++ : this.currentRotation = 0;
+				this.updateBCD(this.blockPos[0], this.blockPos[1]);
+			} else {
+				// Rotate counterclockwise
+				this.currentRotation > 0 ? this.currentRotation-- : this.currentRotation = 3;
+				this.updateBCD(this.blockPos[0], this.blockPos[1]);
 			}
 		}
-
-		this.currentRotation = this.rotation.DOWN;
-
-	}
-
-	getRotation() {
-		return this.rotation.properties[this.currentRotation];
-	};
-	getCurrentRotation() {
-		return this.currentRotation;
-	}
-
-	// Iterate through the rotation states
-	rotate(direction) {
-		if (direction >= 0) {
-			//clockwise
-			this.currentRotation == 3 ? this.currentRotation = 0 : this.currentRotation++;
-			this.updateBCD(this.rotation.properties[this.currentRotation]);
-		} else {
-			//counterclockwise
-			this.currentRotation == 0 ? this.currentRotation = 3 : this.currentRotation--;
-		}
-	}
-
-	// Update the bcd-block positions based on the a-block position
-	updateBCD(obj) {
-		for (var i = 1; i < 4; i++) {
-			this.rotation.properties[this.currentRotation][i].x = obj[0].x + obj[i].xd;
-			this.rotation.properties[this.currentRotation][i].y = obj[0].y + obj[i].yd;
+		// Update the bcd-block positions based on the a-block position
+	updateBCD(x, y) {
+		for (var i = 2; i < this.blockPos.length; i++) {
+			this.blockPos[i] = x + this.deltaPos[this.currentRotation][i - 2];
+			i++;
+			this.blockPos[i] = y + this.deltaPos[this.currentRotation][i - 2];
 		}
 	}
 
@@ -161,653 +58,186 @@ class Piece {
 	}
 
 	render() {
-		var obj = this.rotation.properties[this.currentRotation];
-		for (var i = 0; i < 4; i++) {
-			this.drawCell(obj[i].x, obj[i].y);
+		for (var i = 0; i < this.blockPos.length; i++) {
+			this.drawCell(this.blockPos[i++], this.blockPos[i]);
 		}
 	}
 }
 class Line extends Piece {
 	constructor() {
 		super();
-
-		this.rotation = {
-			DOWN: 0,
-			LEFT: 1,
-			UP: 2,
-			RIGHT: 3,
-			properties: {
-				0: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 3
-					}
-				},
-				1: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: -2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -3,
-						yd: 0
-					}
-				},
-				2: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -3
-					}
-				},
-				3: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 3,
-						yd: 0
-					}
-				}
-			}
-		}
-
-		// Start lines facing right at position 3
-		this.currentRotation = this.rotation.DOWN;
-		this.rotation.properties[this.currentRotation][0].x = 3;
+		// Rotation enum
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		this.deltaPos = [
+			[0, 1, 0, 2, 0, 3], //down
+			[-1, 0, -2, 0, -3, 0], //left
+			[0, -1, 0, -2, 0, -3], //up
+			[1, 0, 2, 0, 3, 0] //right
+		];
+		// Start lines facing down at position 5
+		this.currentRotation = 0;
+		this.blockPos[0] = 5;
 		//update the rest of the squares
-		this.updateBCD(this.rotation.properties[this.currentRotation]);
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
 	}
 }
 class TShape extends Piece {
 	constructor() {
 		super();
-
-		this.rotation = {
-			DOWN: 0,
-			LEFT: 1,
-			UP: 2,
-			RIGHT: 3,
-			properties: {
-				0: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 1
-					}
-				},
-				1: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 1
-					}
-				},
-				2: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: -2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: -1
-					}
-				},
-				3: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: -1
-					}
-				}
-			}
-		}
-
-		// Start lines facing down at position 3
-		this.currentRotation = this.rotation.DOWN;
-		this.rotation.properties[this.currentRotation][0].x = 3;
+		// Rotation enum
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		// Down: 0, Left: 1, Up: 2, Right: 3
+		this.currentRotation = 0;
+		this.deltaPos = [
+			[-1, 1, 0, 1, 1, 1], //down
+			[-1, -1, -1, 0, -1, 1], //left
+			[1, -1, 0, -1, -1, -1], //up
+			[1, 1, 1, 0, 1, -1] //right
+		];
+		// Start lines facing down at position 5
+		this.currentRotation = 0;
+		this.blockPos[0] = 5;
 		//update the rest of the squares
-		this.updateBCD(this.rotation.properties[this.currentRotation]);
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
 	}
 }
 class LShape extends Piece {
 	constructor() {
 		super();
-
-		this.rotation = {
-			DOWN: 0,
-			LEFT: 1,
-			UP: 2,
-			RIGHT: 3,
-			properties: {
-				0: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 2
-					}
-				},
-				1: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: -2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -2,
-						yd: 1
-					}
-				},
-				2: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: -2
-					}
-				},
-				3: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 2,
-						yd: -1
-					}
-				}
-			}
-		}
-
-		// Start lines facing left at position 3
-		this.currentRotation = this.rotation.LEFT;
-		this.rotation.properties[this.currentRotation][0].x = 3;
+		// Rotation enum
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		this.deltaPos = [
+			[1, 0, 1, 1, 1, 2], //down
+			[0, 1, -1, 1, -2, 1], //left
+			[-1, 0, -1, -1, -1, -2], //up
+			[0, -1, 1, -1, 2, -1] //right
+		];
+		// Start lines facing down at position 5
+		this.currentRotation = 0;
+		this.blockPos[0] = 5;
 		//update the rest of the squares
-		this.updateBCD(this.rotation.properties[this.currentRotation]);
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
 	}
 }
 class ReverseL extends Piece {
 	constructor() {
 		super();
-
-		this.rotation = {
-			DOWN: 0,
-			LEFT: 1,
-			UP: 2,
-			RIGHT: 3,
-			properties: {
-				0: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 2
-					}
-				},
-				1: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: -2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -2,
-						yd: -1
-					}
-				},
-				2: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -2
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: -2
-					}
-				},
-				3: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 2,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 2,
-						yd: 1
-					}
-				}
-			}
-		}
-
-		// Start lines facing right at position 3
-		this.currentRotation = this.rotation.RIGHT;
-		this.rotation.properties[this.currentRotation][0].x = 3;
+		// Rotation enum
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		this.deltaPos = [
+			[-1, 0, -1, 1, -1, 2], //down
+			[0, -1, -1, -1, -2, -1], //left
+			[1, 0, 1, -1, 1, -2], //up
+			[0, 1, 1, 1, 2, 1] //right
+		];
+		// Start lines facing down at position 5
+		this.currentRotation = 0;
+		this.blockPos[0] = 5;
 		//update the rest of the squares
-		this.updateBCD(this.rotation.properties[this.currentRotation]);
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
 	}
 }
 class Square extends Piece {
 	constructor() {
 		super();
-
-		this.rotation = {
-			DOWN: 0,
-			LEFT: 1,
-			UP: 2,
-			RIGHT: 3,
-			properties: {
-				0: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 1
-					}
-				},
-				1: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -1
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: -1,
-						yd: -1
-					}
-				},
-				2: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: -1
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 0
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: -1
-					}
-				},
-				3: {
-					0: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 0
-					},
-					1: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 0
-					},
-					2: {
-						x: 0,
-						y: 0,
-						xd: 0,
-						yd: 1
-					},
-					3: {
-						x: 0,
-						y: 0,
-						xd: 1,
-						yd: 1
-					}
-				}
-			}
-		}
-
-		// Start lines facing right at position 3
-		this.currentRotation = this.rotation.RIGHT;
-		this.rotation.properties[this.currentRotation][0].x = 3;
+		// Rotation enum
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		this.deltaPos = [
+			[0, 1, 1, 0, 1, 1] //down
+		];
+		// Start lines facing down at position 5
+		this.currentRotation = 0;
+		this.blockPos[0] = 5;
 		//update the rest of the squares
-		this.updateBCD(this.rotation.properties[this.currentRotation]);
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
+	}
+	rotate() {
+		console.log("Squares don't rotate...");
 	}
 }
-
-var lineTest = new Line();
-var lTest = new LShape();
-var rLTest = new ReverseL();
-var tTest = new TShape();
 
 function createCanvas() {
 	canvas = document.createElement("canvas");
 	ctx = canvas.getContext("2d");
-	canvas.width = 512;
-	canvas.height = 480;
+	canvas.width = 400;
+	canvas.height = 600;
 	document.body.appendChild(canvas);
+}
+
+function getRandomPiece() {
+	switch (Math.floor(Math.random() * 100) % 5) {
+		case 0:
+			return new Line();
+			break;
+		case 1:
+			return new LShape();
+			break;
+		case 2:
+			return new ReverseL();
+			break;
+		case 3:
+			return new Square();
+			break;
+		case 4:
+			return new TShape();
+			break;
+		default:
+			console.log("Error getting piece");
+	}
+}
+
+function init() {
+	createCanvas();
+	currentPiece = getRandomPiece();
+	gameLoop = setInterval(update, loopSpeed);
+}
+
+function update() {
+	runTime += loopSpeed;
+	if(runTime >= speed) {
+		runTime = 0;
+		currentPiece.move(0, 1);
+	}
 	render();
 }
 
 function render() {
-
 	//background
 	ctx.fillStyle = "#FFFFFF";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	// Current Piece
-	tTest.render();
+	currentPiece.render();
 }
+
+$(document).keydown(function(e) {
+	switch (e.keyCode) {
+		// X Key
+		case 88:
+			currentPiece.rotate(true);
+			break;
+			// Z Key
+		case 90:
+			currentPiece.rotate(false);
+			break;
+			// Left Key
+		case 37:
+			currentPiece.move(-1, 0);
+			break;
+			// Up Key
+		case 38:
+			break;
+			// Right Key
+		case 39:
+			currentPiece.move(1, 0);
+			break;
+			// Down Key
+		case 40:
+			currentPiece.move(0, 1);
+			break;
+	}
+});
+
+$(document).ready(function() {
+	init();
+});
