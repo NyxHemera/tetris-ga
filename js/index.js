967e364c7d2830d75b751359a72c28cce5a035ee
Jordan Rust <jordan.rust92@gmail.com>
2016-04-01 10:36:55 -0400

    Fixed Rotation with Kicking!
    Finally fixed the rotation clipping bug, hopefully for good. Now when you rotate, the rotation method checks three positions. The first is normal rotation, if that doesn't work, it checks one block over on the x axis, if that doesn't work, it check one block left on the x axis from the original rotation. If none work, the block doesn't rotate.
    
    Things to work on:
    
    - The line still uses the old rotation. It needs its own rotation method, overriding the main Piece one.
    
    - The line also still uses the old layout for being rendered, with the axis of rotation on the outermost block. This needs to be changed to a custom rotation, different than the other 3-length pieces.
    
    - Z and S shapes start facing the wrong direction. Quick fix is changing the initial rotation.
    
    - If you rotate while at the top or bottom of the screen, the game crashes. This is because of indexOf in the blockCollision function is being run on an array that doesn't exist. The bottom will be fixed by adding kicking on the bottom, similar to the sides. I don't want kicking at the top, but it should be able to clip through the top. The pieces should always start in full view though.
    
    - Losing conditions : This still needs to be worked on. This should only be tested in the grabNewPiece method, to prevent issues with rotation when a piece is first loaded. I also need to take into account piece clipping when they load, and let that signal a losing condition.

--- js/index.js
+++ js/index.js
@@ -25,38 +25,77 @@ class Piece {
 			[] //right
 		];
 	}
-	move(x, y) {
-			var fCollision = this.checkFrameCollision(this.blockPos);
-			//console.log("fCollision");
-			//console.log(fCollision);
-			// If moving down, checkBlockCollision, else don't...
-			var bCollision = this.checkBlockCollision(this.blockPos);
-			//console.log("bCollision");
-			//console.log(bCollision);
-			// If not colliding with the bottom or another piece:
-			if(!fCollision[2] && !bCollision[2]) {
-				this.blockPos[1] += y; // Move Down
-				if(x > 0 && !fCollision[1] && !bCollision[1]) { // If not colliding with the right wall or a piece to the right
-					this.blockPos[0] += x; // Move Right
-				}else if(x < 0 && !fCollision[0] && !bCollision[0]) { // If not colliding with the left wall or a piece to the left
-					this.blockPos[0] += x; // Move Left
-				}
-			} else {
-				// Bottom Collision or Piece-on-Piece Collision
-				grabNewPiece();
-			}
+	move(x, y, bool) { // Move will return array of true on either a successful move or no move, or false on collision
+		var fCollision = this.checkFrameCollision(this.blockPos);
+		var bCollision = this.checkBlockCollision(this.blockPos);
+		var success = [!x, !y]; // If not moving, set true, if moving set false. Numbers other than 0 are truthy.
+		if(bool) { // Bool allows for forced movement, no matter the collision
+			this.blockPos[0] += x;
+			this.blockPos[1] += y;
 			this.updateBCD(this.blockPos[0], this.blockPos[1]);
+			return [true, true];
 		}
+
+		// If not colliding with the bottom or another piece:
+		if (!fCollision[2] && !bCollision[2]) {
+			this.blockPos[1] += y; // Move Down
+			success[1] = true;
+			if (x > 0 && !fCollision[1] && !bCollision[1]) { // If not colliding with the right wall or a piece to the right
+				this.blockPos[0] += x; // Move Right
+				success[0] = true;
+			} else if (x < 0 && !fCollision[0] && !bCollision[0]) { // If not colliding with the left wall or a piece to the left
+				this.blockPos[0] += x; // Move Left
+				success[0] = true;
+			}
+		} else {
+			// Bottom Collision or Piece-on-Piece Collision
+			grabNewPiece();
+		}
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
+		return success;
+	}
 		// Iterate through the rotation states
 	rotate(direction) {
+		var originalRotation = this.currentRotation;
+
 		if (direction) {
 			// Rotate clockwise
 			this.currentRotation < 3 ? this.currentRotation++ : this.currentRotation = 0;
 			this.updateBCD(this.blockPos[0], this.blockPos[1]);
+			if(this.checkBlockClipping(this.blockPos)) { // Check if normal rotation gave a good result
+				// If it didn't...kick to the right
+				this.move(1,0,true);
+				this.updateBCD(this.blockPos[0], this.blockPos[1]);
+				if(this.checkBlockClipping(this.blockPos)) { // Check if kicking to the right gives a good result
+					// If that didn't...kick to the left
+					this.move(-2,0,true);
+					this.updateBCD(this.blockPos[0], this.blockPos[1]);
+				}if(this.checkBlockClipping(this.blockPos)) { // Check if kicking to the left gives a good result
+					// If that didn't...move back to the original rotation, don't rotate.
+					this.move(1,0,true);
+					this.currentRotation = originalRotation;
+					this.updateBCD(this.blockPos[0], this.blockPos[1]);
+				}
+			}
 		} else {
 			// Rotate counterclockwise
 			this.currentRotation > 0 ? this.currentRotation-- : this.currentRotation = 3;
 			this.updateBCD(this.blockPos[0], this.blockPos[1]);
+			if(this.checkBlockClipping(this.blockPos)) { // Check if normal rotation gave a good result
+				// If it didn't...kick to the right
+				this.move(1,0,true);
+				this.updateBCD(this.blockPos[0], this.blockPos[1]);
+				if(this.checkBlockClipping(this.blockPos)) { // Check if kicking to the right gives a good result
+					// If that didn't...kick to the left
+					this.move(-2,0,true);
+					this.updateBCD(this.blockPos[0], this.blockPos[1]);
+				}if(this.checkBlockClipping(this.blockPos)) { // Check if kicking to the left gives a good result
+					// If that didn't...move back to the original rotation, don't rotate.
+					this.move(1,0,true);
+					this.currentRotation = originalRotation;
+					this.updateBCD(this.blockPos[0], this.blockPos[1]);
+				}
+			}
 		}
 	}
 	// Update the bcd-block positions based on the a-block position
@@ -67,49 +106,43 @@ class Piece {
 			this.blockPos[i] = y + this.deltaPos[this.currentRotation][i - 2];
 		}
 	}
-	drawCell(x, y) {
-		ctx.fillStyle = "blue";
-		ctx.fillRect(x * bw, y * bw, bw, bw);
-		ctx.strokeStyle = "white";
-		ctx.strokeRect(x * bw, y * bw, bw, bw);
-	}
 	checkFrameCollision(pos) {
 		var collision = [false, false, false];
-		for(var i=0; i<pos.length; i++) {
+		for (var i = 0; i < pos.length; i++) {
 			// Run X collision
 			!collision[0] ? collision[0] = pos[i] <= 0 : ""; // Collides with Left Wall
-			!collision[1] ? collision[1] = pos[i] >= (canvas.width/bw - 1) : ""; // Collides with Right Wall
+			!collision[1] ? collision[1] = pos[i] >= (canvas.width / bw - 1) : ""; // Collides with Right Wall
 			i++; //Must go here so it always increments
 			// Run Y collision
-			!collision[2] ? collision[2] = pos[i] >= (canvas.height/bw - 1) : ""; // Collides with Floor
+			!collision[2] ? collision[2] = pos[i] >= (canvas.height / bw - 1) : ""; // Collides with Floor
 		}
 		return collision;
 	}
-	/*checkBlockCollision(pos) {
-		// Assume block moving down
-		for(var i=0; i<pos.length; i++) {
-			if(columnTops[pos[i]] === (pos[i+1] + 1)) {
-				return true;
-			}
-		}
-		return false;
-	}*/
 	checkBlockCollision(pos) {
 		var collision = [false, false, false];
-		for(var i=0; i<pos.length; i++) {
+		for (var i = 0; i < pos.length; i++) {
 
-			!collision[0] ? collision[0] = blockGrid[pos[i+1]].indexOf(pos[i]-1) != -1 : ""; // Collision Left; If there is a block to the left(x-1)
-			!collision[1] ? collision[1] = blockGrid[pos[i+1]].indexOf(pos[i]+1) != -1 : ""; // Collision Right; If there is a block to the right(x+1)
+			!collision[0] ? collision[0] = blockGrid[pos[i + 1]].indexOf(pos[i] - 1) != -1 : ""; // Collision Left; If there is a block to the left(x-1)
+			!collision[1] ? collision[1] = blockGrid[pos[i + 1]].indexOf(pos[i] + 1) != -1 : ""; // Collision Right; If there is a block to the right(x+1)
 			var row;
-			blockGrid[pos[i+1]+ 1] != undefined ? row = blockGrid[pos[i+1]+ 1] : row = [];
+			blockGrid[pos[i + 1] + 1] != undefined ? row = blockGrid[pos[i + 1] + 1] : row = [];
 			!collision[2] ? collision[2] = row.indexOf(pos[i]) != -1 : ""; // Collision Bottom; If there is a block below(y+1, x)
 			i++;
 		}
 		return collision;
 	}
+	checkBlockClipping(pos) { // Checks to see if the Piece is occupying the same space as other blocks
+		for(var i=0; i< pos.length; i++) {
+			if(blockGrid[pos[i+1]].indexOf(pos[i]) != -1 || pos[i] < 0 || pos[i] >= canvas.width/bw) {
+				return true; // If clipping with block, leftframe, or right rame, return true, else continue checking
+			}
+			i++;
+		}
+		return false;
+	}
 	render() {
 		for (var i = 0; i < this.blockPos.length; i++) {
-			this.drawCell(this.blockPos[i++], this.blockPos[i]);
+			drawCell(this.blockPos[i++], this.blockPos[i]);
 		}
 	}
 }
@@ -139,13 +172,13 @@ class TShape extends Piece {
 		// Down: 0, Left: 1, Up: 2, Right: 3
 		this.currentRotation = 0;
 		this.deltaPos = [
-			[-1, 1, 0, 1, 1, 1], //down
-			[-1, -1, -1, 0, -1, 1], //left
-			[1, -1, 0, -1, -1, -1], //up
-			[1, 1, 1, 0, 1, -1] //right
+			[0, 1, 0, -1, 1, 0], //down
+			[1, 0, -1, 0, 0, -1], //left
+			[0, -1, 0, 1, -1, 0], //up
+			[-1, 0, 1, 0, 0, 1] //right
 		];
-		// Start lines facing down at position 5
-		this.currentRotation = 0;
+		// Start lines facing right at position 5
+		this.currentRotation = 3;
 		this.blockPos[0] = 5;
 		//update the rest of the squares
 		this.updateBCD(this.blockPos[0], this.blockPos[1]);
@@ -157,13 +190,13 @@ class LShape extends Piece {
 		// Rotation enum
 		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
 		this.deltaPos = [
-			[1, 0, 1, 1, 1, 2], //down
-			[0, 1, -1, 1, -2, 1], //left
-			[-1, 0, -1, -1, -1, -2], //up
-			[0, -1, 1, -1, 2, -1] //right
+			[0,-1,0,1,1,1], //down
+			[1,0,-1,0,-1,1], //left
+			[0,1,0,-1,-1,-1], //up
+			[-1,0,1,0,1,-1] //right
 		];
-		// Start lines facing down at position 5
-		this.currentRotation = 0;
+		// Start lines facing left at position 5
+		this.currentRotation = 1;
 		this.blockPos[0] = 5;
 		//update the rest of the squares
 		this.updateBCD(this.blockPos[0], this.blockPos[1]);
@@ -175,13 +208,13 @@ class ReverseL extends Piece {
 		// Rotation enum
 		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
 		this.deltaPos = [
-			[-1, 0, -1, 1, -1, 2], //down
-			[0, -1, -1, -1, -2, -1], //left
-			[1, 0, 1, -1, 1, -2], //up
-			[0, 1, 1, 1, 2, 1] //right
+			[0,1,0,-1,1,-1], //down
+			[-1,0,1,0,1,1], //left
+			[0,-1,0,1,-1,1], //up
+			[1,0,-1,0,-1,-1] //right
 		];
-		// Start lines facing down at position 5
-		this.currentRotation = 0;
+		// Start lines facing left at position 5
+		this.currentRotation = 1;
 		this.blockPos[0] = 5;
 		//update the rest of the squares
 		this.updateBCD(this.blockPos[0], this.blockPos[1]);
@@ -205,8 +238,44 @@ class Square extends Piece {
 		console.log("Squares don't rotate...");
 	}
 }
+class SShape extends Piece {
+	constructor() {
+		super();
+		// Rotation enum
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		this.deltaPos = [
+			[0,-1,1,1,1,0], //down
+			[-1,0,1,-1,0,-1], //left
+			[0,1,-1,-1,-1,0], //up
+			[1,0,-1,1,0,1] //right
+		];
+		// Start lines facing down at position 5
+		this.currentRotation = 0;
+		this.blockPos[0] = 5;
+		//update the rest of the squares
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
+	}
+}
+class ZShape extends Piece {
+	constructor() {
+		super();
+		// Rotation enum
+		this.blockPos = [0, 0, 0, 0, 0, 0, 0, 0];
+		this.deltaPos = [
+			[0,1,1,-1,1,0], //down
+			[1,0,-1,-1,0,-1], //left
+			[0,-1,-1,1,-1,0], //up
+			[-1,0,1,1,0,1] //right
+		];
+		// Start lines facing down at position 5
+		this.currentRotation = 0;
+		this.blockPos[0] = 5;
+		//update the rest of the squares
+		this.updateBCD(this.blockPos[0], this.blockPos[1]);
+	}
+}
 
-function addBlock(x,y) {
+function addBlock(x, y) {
 	blockGrid[y].push(x);
 }
 
@@ -218,20 +287,20 @@ function removeRow(rowIndex) {
 }
 
 function checkRows() {
-	for(var i=0; i<blockGrid.length; i++) {
-		blockGrid[i].length >= canvas.width/bw ? removeRow(i) : "";
+	for (var i = 0; i < blockGrid.length; i++) {
+		blockGrid[i].length >= canvas.width / bw ? removeRow(i) : "";
 	}
 }
 
 function renderBlocks() {
-	for(var i=0; i<blockGrid.length; i++) {
-		for(var j=0; j<blockGrid[i].length; j++) {
+	for (var i = 0; i < blockGrid.length; i++) {
+		for (var j = 0; j < blockGrid[i].length; j++) {
 			drawCell(blockGrid[i][j], i);
 		}
 	}
 }
 
-function 	drawCell(x, y) {
+function drawCell(x, y) {
 	ctx.fillStyle = "blue";
 	ctx.fillRect(x * bw, y * bw, bw, bw);
 	ctx.strokeStyle = "white";
@@ -247,8 +316,8 @@ function createCanvas() {
 }
 
 function grabNewPiece() {
-	for(var i=0; i<currentPiece.blockPos.length; i++) {
-		addBlock(currentPiece.blockPos[i], currentPiece.blockPos[i+1]);
+	for (var i = 0; i < currentPiece.blockPos.length; i++) {
+		addBlock(currentPiece.blockPos[i], currentPiece.blockPos[i + 1]);
 		i++;
 	}
 	checkRows();
@@ -257,7 +326,7 @@ function grabNewPiece() {
 }
 
 function getRandomPiece() {
-	switch (Math.floor(Math.random() * 100) % 5) {
+	switch (Math.floor(Math.random() * 100) % 7) {
 		case 0:
 			return new Line();
 			break;
@@ -273,6 +342,12 @@ function getRandomPiece() {
 		case 4:
 			return new TShape();
 			break;
+		case 5:
+			return new SShape();
+			break;
+		case 6:
+			return new ZShape();
+			break;
 		default:
 			console.log("Error getting piece");
 	}
@@ -281,13 +356,14 @@ function getRandomPiece() {
 function init() {
 	createCanvas();
 	initializeVariables();
-	currentPiece = getRandomPiece();
+	//currentPiece = getRandomPiece();
+	currentPiece = new LShape();
 	gameLoop = setInterval(update, loopSpeed);
 }
 
 function initializeVariables() {
 	console.log(blockGrid);
-	for(var i=0; i<canvas.height/bw; i++) {
+	for (var i = 0; i < canvas.height / bw; i++) {
 		blockGrid.push([]);
 	}
 	console.log(blockGrid);
@@ -295,9 +371,9 @@ function initializeVariables() {
 
 function update() {
 	runTime += loopSpeed;
-	if(runTime >= speed) {
+	if (runTime >= speed) {
 		runTime = 0;
-		currentPiece.move(0, 1);
+		currentPiece.move(0, 1, false);
 	}
 	render();
 }
@@ -325,18 +401,18 @@ $(document).keydown(function(e) {
 			break;
 			// Left Key
 		case 37:
-			currentPiece.move(-1, 0);
+			currentPiece.move(-1, 0, false);
 			break;
 			// Up Key
 		case 38:
 			break;
 			// Right Key
 		case 39:
-			currentPiece.move(1, 0);
+			currentPiece.move(1, 0, false);
 			break;
 			// Down Key
 		case 40:
-			currentPiece.move(0, 1);
+			currentPiece.move(0, 1, false);
 			break;
 	}
 });
